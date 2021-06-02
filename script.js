6f4a0160608fe9112a2ca92810657197721d21e5
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2021-06-02 20:58:34 +0530

    Added Basic layout of Home Page

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//  ------------------  Navigation Bar starts ------------------ //
+
+function resizeNav() {
+    // Set the nav height to fill the window
+    $("#nav-fullscreen").css({"height": window.innerHeight});
+
+    // Set the circle radius to the length of the window diagonal,
+    // this way we're only making the circle as big as it needs to be.
+    const radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
+    const diameter = radius * 2;
+    $("#nav-overlay").width(diameter);
+    $("#nav-overlay").height(diameter);
+    $("#nav-overlay").css({"margin-top": -radius, "margin-left": -radius});
+}
+
+// Set up click and window resize callbacks, then init the nav.
+$(document).ready(function() {
+    $("#nav-toggle").click(function() {
+        $("#nav-toggle, #nav-overlay, #nav-fullscreen").toggleClass("open");
+    });
+    $(window).resize(resizeNav);
+    resizeNav();
+});
+
+//  ------------------  Navigation Bar ends ------------------ //
+
